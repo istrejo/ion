6ec477ddc0a18c4db88b28690a92f5fe05decf0b
istrejo <istrejo2106@gmail.com>
2022-09-25 01:51:00 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-nicobytes-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
